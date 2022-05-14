27da495b21830088fb03651620a883b71f63156e
guyeilon <guyeilon84@gmail.com>
2022-05-15 00:58:41 +0300

    delete files

--- styled-component/src/App.js
+++ styled-component/src/App.js
@@ -2,13 +2,9 @@ import { ThemeProvider } from 'styled-components';
 
 import { theme } from './App/theme';
 import Header from './components/Header';
-import Card from './components/Card';
 import { Container } from './components/styles/Container.styled';
 import GlobalStyles from './components/styles/Global';
 
-import content from './Data/content';
-
-import { Flex } from './components/styles/Flex.styled';
 import MainContainer from './components/MainContainer';
 
 const App = () => {
